a04d58613f8b1d9528212aa13dd64fb562349298
mikan <yutakakato@icloud.com>
2017-04-30 22:51:11 +0900

    add ListsAndRecursion-5 / all?

--- lib/ListsAndRecursion-5/enum.ex
+++ lib/ListsAndRecursion-5/enum.ex
@@ -1,6 +1,7 @@
 defmodule MyEnum do
+  def all?(enumerable, fun), do: filter(enumerable, fun) != []
   def filter(enumerable, fun), do: _filter(enumerable, [], fun)
-  defp _filter([], filtered, fun), do: filtered;
+  defp _filter([], filtered, _), do: filtered;
   defp _filter([head | tail], filtered, fun) do
     if fun.(head) do
       _filter(tail, filtered ++ [head], fun)
@@ -9,5 +10,3 @@ defmodule MyEnum do
     end
   end
 end
-
-IO.inspect MyEnum.filter [1,2,3,4,5], &(&1 > 3) # [4, 5]
--- test/ListsAndRecursion-5/enum_test.exs
+++ test/ListsAndRecursion-5/enum_test.exs
@@ -1,6 +1,10 @@
 defmodule MyEnumTest do
   use ExUnit.Case
-  test "normal case" do
+  test "all? normal case" do
+    assert MyEnum.all?([1, 2, 3, 4, 5], &(&1 <= 5)) == true
+    assert MyEnum.all?([1, 2, 3, 4, 5], &(&1 == 0)) == false
+  end
+  test "filter normal case" do
     assert MyEnum.filter([1, 2, 3, 4, 5], &(&1 > 3)) == [4, 5]
   end
 end
