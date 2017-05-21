fcd06a7497e801ac86322472c5303257d40ed268
mikan <yutakakato@icloud.com>
2017-05-21 22:51:11 +0900

    add ListsAndRecursion-7

---
+++ lib/ListsAndRecursion-7/prime.ex
@@ -0,0 +1,13 @@
+defmodule Span do
+  # same as ListsAndRecursion-4
+  def span(from, to) when to < from, do: []
+  def span(from, to), do: [from | span(from + 1, to)]
+end
+
+defmodule Prime do
+  def prime(n), do: for i <- Span.span(2, n), prime?(i), do: i
+  defp remzero(n), do: for i <- Span.span(1, n), rem(n, i) == 0, do: i
+  defp prime?(n), do: Enum.count(remzero(n)) == 2
+end
+
+IO.inspect Prime.prime(20) # [2, 3, 5, 7, 11, 13, 17, 19]
