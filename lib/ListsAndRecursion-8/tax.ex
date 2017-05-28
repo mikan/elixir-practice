0de2ff67951a3a545b7b3585657a395b95144392
mikan <yutakakato@icloud.com>
2017-05-28 22:02:25 +0900

    update ListsAndRecursion-8

--- lib/ListsAndRecursion-8/tax.ex
+++ lib/ListsAndRecursion-8/tax.ex
@@ -1,8 +1,8 @@
 defmodule Tax do
-  def tax_ins(rates, orders), do: orders |> Stream.map(fn v -> tax_in(rates, v, v[:ship_to]) end) |> Enum.to_list
-  defp tax_in(rates, order, state) when state == :NC, do: Keyword.update!(order, :net_amount, fn v -> v * (1 + rates[state]) end)
-  defp tax_in(rates, order, state) when state == :TX, do: Keyword.update!(order, :net_amount, fn v -> v * (1 + rates[state]) end)
-  defp tax_in(_, order, _), do: order
+  def tax_ins(rates, orders), do: orders |> Stream.map(fn v -> tax_in(v, rates, v[:ship_to]) end) |> Enum.to_list
+  defp tax_in(order, rates, state) when state == :NC, do: Keyword.update!(order, :net_amount, fn v -> v * (1 + rates[state]) end)
+  defp tax_in(order, rates, state) when state == :TX, do: Keyword.update!(order, :net_amount, fn v -> v * (1 + rates[state]) end)
+  defp tax_in(order,_ , _), do: order
 end
 
 tax_rates = [ NC: 0.075, TX: 0.08 ]
@@ -17,3 +17,11 @@ orders = [
   [ id: 130, ship_to: :NC, net_amount: 50.00 ]]
 
 IO.inspect Tax.tax_ins(tax_rates, orders)
+# [[id: 123, ship_to: :NC, net_amount: 107.5],
+#  [id: 124, ship_to: :OK, net_amount: 35.5],
+#  [id: 125, ship_to: :TX, net_amount: 25.92],
+#  [id: 126, ship_to: :TX, net_amount: 48.384],
+#  [id: 127, ship_to: :NC, net_amount: 26.875],
+#  [id: 128, ship_to: :MA, net_amount: 10.0],
+#  [id: 129, ship_to: :CA, net_amount: 102.0],
+#  [id: 130, ship_to: :NC, net_amount: 53.75]]
