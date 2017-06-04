f14d10db99d81c1b28f50146444ce6fafab3cb1c
mikan <yutakakato@icloud.com>
2017-06-04 22:18:29 +0900

    add StringsAndBinaries-1

---
+++ lib/StringsAndBinaries-1/printable.ex
@@ -0,0 +1,18 @@
+defmodule MyString1 do
+  def printable?([]), do: true
+  def printable?(enumerable), do: Enum.all?(enumerable, &(_printable? &1))
+  defp _printable?(c) when c >= 32 and c <= 126, do: true
+  defp _printable?(_), do: false
+end
+
+# true
+IO.puts MyString1.printable? ''
+IO.puts MyString1.printable? 'a'
+IO.puts MyString1.printable? 'b'
+IO.puts MyString1.printable? 'ab'
+
+# false
+IO.puts MyString1.printable? '\a'
+IO.puts MyString1.printable? '\d'
+IO.puts MyString1.printable? 'あ'
+IO.puts MyString1.printable? 'aあ'
