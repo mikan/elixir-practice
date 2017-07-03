0bbe46f080ceecab51edfbefbff4e5ef61453474
mikan <yutakakato@icloud.com>
2017-07-03 10:41:19 +0900

    fix new func

--- lib/StringsAndBinaries-7/tax.ex
+++ lib/StringsAndBinaries-7/tax.ex
@@ -11,17 +11,17 @@ defmodule MyString7 do
   defp load(f), do: File.stream!(f) |> Stream.filter_map(&(not String.starts_with?(&1, "id")), &String.trim/1)
   defp parse(line), do: new(String.split(line, ","))
   defp new(data), do: [
-    {:id, Enum.at(data, 0)},
-    {:ship_to, String.to_atom(Enum.at(data, 1))},
-    {:net_amount, Enum.at(data, 2)}]
+    {:id, String.to_integer(Enum.at(data, 0))},
+    {:ship_to, String.to_atom(String.slice(Enum.at(data, 1), 1, 2))},
+    {:net_amount, String.to_float(Enum.at(data, 2))}]
 end
 
 IO.inspect MyString7.tax_from_file(tax_rates, "lib/StringsAndBinaries-7/sales.csv")
-# [[id: "123", ship_to: :":NC", net_amount: "100.00", total_amount: "100.00"],
-#  [id: "124", ship_to: :":OK", net_amount: "35.50", total_amount: "35.50"],
-#  [id: "125", ship_to: :":TX", net_amount: "24.00", total_amount: "24.00"],
-#  [id: "126", ship_to: :":TX", net_amount: "44.80", total_amount: "44.80"],
-#  [id: "127", ship_to: :":NC", net_amount: "25.00", total_amount: "25.00"],
-#  [id: "128", ship_to: :":MA", net_amount: "10.00", total_amount: "10.00"],
-#  [id: "129", ship_to: :":CA", net_amount: "102.00", total_amount: "102.00"],
-#  [id: "120", ship_to: :":NC", net_amount: "50.00", total_amount: "50.00"]]
+# [[id: 123, ship_to: :NC, net_amount: 100.0, total_amount: 107.5],
+#  [id: 124, ship_to: :OK, net_amount: 35.5, total_amount: 35.5],
+#  [id: 125, ship_to: :TX, net_amount: 24.0, total_amount: 25.92],
+#  [id: 126, ship_to: :TX, net_amount: 44.8, total_amount: 48.384],
+#  [id: 127, ship_to: :NC, net_amount: 25.0, total_amount: 26.875],
+#  [id: 128, ship_to: :MA, net_amount: 10.0, total_amount: 10.0],
+#  [id: 129, ship_to: :CA, net_amount: 102.0, total_amount: 102.0],
+#  [id: 120, ship_to: :NC, net_amount: 50.0, total_amount: 53.75]]
