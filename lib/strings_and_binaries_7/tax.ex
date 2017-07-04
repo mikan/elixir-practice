3377014a696367a89f1b929fde2a6563af54565a
mikan <yutakakato@icloud.com>
2017-07-04 10:53:48 +0900

    remove duplicate module

--- lib/strings_and_binaries_7/tax.ex
+++ lib/strings_and_binaries_7/tax.ex
@@ -1,10 +1,4 @@
-defmodule ListsAndRecursion8 do
-  def tax_ins(orders, rates), do: orders |> Enum.map(&tax_in(&1, rates[&1[:ship_to]]))
-  defp tax_in(order, nil), do: order ++ [total_amount: order[:net_amount]]
-  defp tax_in(order, rate), do: order ++ [total_amount: order[:net_amount] * (1 + rate)]
-end
-tax_rates = [ NC: 0.075, TX: 0.08 ]
-
+# Requires ListsAndRecursion8
 defmodule StringsAndBinaries7 do
   def tax_from_file(rates, filename), do: load(filename) |> Enum.map(&parse/1) |> ListsAndRecursion8.tax_ins(rates)
   defp load(f), do: File.stream!(f) |> Stream.filter_map(&(not String.starts_with?(&1, "id")), &String.trim/1)
@@ -15,6 +9,7 @@ defmodule StringsAndBinaries7 do
     {:net_amount, Enum.at(data, 2) |> String.to_float}]
 end
 
+tax_rates = [ NC: 0.075, TX: 0.08 ]
 IO.inspect StringsAndBinaries7.tax_from_file(tax_rates, "lib/strings_and_binaries_7/sales.csv")
 # [[id: 123, ship_to: :NC, net_amount: 100.0, total_amount: 107.5],
 #  [id: 124, ship_to: :OK, net_amount: 35.5, total_amount: 35.5],
