d77fe6b40d85e649bfbd294cfbf593412a4aa4a4
mikan <mikan@aosn.ws>
2017-07-23 21:50:11 +0900

    complete organizing_a_project_1

--- test/organizing_a_project_1/issues_test.exs
+++ test/organizing_a_project_1/issues_test.exs
@@ -1,6 +1,6 @@
 defmodule IssuesTest do
   use ExUnit.Case
-  doctest ElixirPractice.OrganizingAProject1.CLI
+  doctest ElixirPractice.OrganizingAProject1
 
   test "the truth" do
     assert 1 + 1 == 2
