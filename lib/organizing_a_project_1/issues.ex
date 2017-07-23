a14fd25020a52b3427222b81800b55442bbed5e8
mikan <mikan@aosn.ws>
2017-07-23 21:39:42 +0900

    add progress of organizing_a_project_1

---
+++ lib/organizing_a_project_1/issues.ex
@@ -0,0 +1,3 @@
+defmodule ElixirPractice.OrganizingAProject1 do
+
+end
---
+++ test/organizing_a_project_1/issues_test.exs
@@ -0,0 +1,8 @@
+defmodule IssuesTest do
+  use ExUnit.Case
+  doctest ElixirPractice.OrganizingAProject1.CLI
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
