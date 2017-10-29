6563d6960d607742d2a43afdf7c2e269d8e4f9b7
mikan <mikan@aosn.ws>
2017-10-29 22:01:40 +0900

    add otp_servers_1

---
+++ lib/otp_servers_1/server.ex
@@ -0,0 +1,19 @@
+defmodule OTPServer1 do
+  use GenServer
+
+  def handle_call(:pop, _from, [head | tail]) do
+    { :reply, head, tail }
+  end
+end
+
+# iex(1)> c("lib/otp_servers_1/server.ex")
+# [OTPServer1]
+# iex(2)> { :ok, pid } = GenServer.start_link(OTPServer1, [5, "cat", 9])
+# {:ok, #PID<0.175.0>}
+#  iex(3)> GenServer.call(pid, :pop)
+# 5
+# iex(4)> GenServer.call(pid, :pop)
+# "cat"
+# iex(5)> GenServer.call(pid, :pop)
+# 9
+# iex(6)>
