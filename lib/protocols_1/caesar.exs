08e2ce2cdcb15ceb4cd45742b9435cc081f9a4b7
mikan <mikan@aosn.ws>
2017-12-24 22:55:53 +0900

    update protocols_1

--- lib/protocols_1/caesar.exs
+++ lib/protocols_1/caesar.exs
@@ -4,27 +4,18 @@ defprotocol Caesar do
 end
 
 defimpl Caesar, for: List do
-  def encrypt(string, shift) do
-    ListsAndRecursion3.caesar(string, shift)
-  end
-  def rot13(string) do
-    encrypt(string, 13)
-  end
+  def encrypt(string, shift), do: ListsAndRecursion3.caesar(string, shift)
+  def rot13(string), do: encrypt(string, 13)
 end
 
 defimpl Caesar, for: BitString do
-  def encrypt(string, shift) do
-    to_string(ListsAndRecursion3.caesar(to_charlist(string), shift))
-
-  end
-  def rot13(string) do
-    encrypt(string, 13)
-  end
+  def encrypt(string, shift), do: to_string(ListsAndRecursion3.caesar(to_charlist(string), shift))
+  def rot13(string), do: encrypt(string, 13)
 end
 
 defmodule ListsAndRecursion3 do
   def caesar([], _), do: []
-  def caesar([head|tail], y), do: [loop(head, y) | caesar(tail, y)]
+  def caesar([head | tail], y), do: [loop(head, y) | caesar(tail, y)]
   defp loop(x, y), do: rem(((x - 97) + y), 26) + 97
 end
 
